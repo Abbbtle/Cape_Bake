3c6558ef5777649fe80b06833510250b846b1df7
Kenneth Basjan <kennethbasjan97@gmail.com>
2023-10-05 11:08:26 +0200

    Hero section is responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import pretzel from '../assets/Images/Images/pretzel.gif'
+import React from 'react';
+import pretzel from '../assets/Images/Images/pretzel.gif';
 
 const Hero = () => {
   return (
     <div name="hero" className='bg-[#e65828] h-[100vh] w-auto'>
-      <div className='md:flex justify-between items-center py-40 mx-10'>
+      <div className='md:flex md:justify-between items-center py-32 md:py-40 mx-10'>
 
         <div className='flex flex-col text-white space-y-5 md:space-y-0'>
-          <h1 className='text-5xl text-center md:text-start md:text-8xl font-medium'>Delicious Baked Goods<br/> and Inviting<br/> Atmosphere</h1>
-          <p className='text-xl text-center md:text-start font-light'>
+          <h1 className='text-5xl text-center md:text-start md:text-8xl font-bold'>Delicious Baked Goods<br/> and Inviting<br/> Atmosphere</h1>
+          <p className='text-xl text-center md:text-start font-medium'>
             Welcome to our bakery, where you'll find a warm and inviting atmosphere paired with our delicious selection of freshly 
             baked goods.
           </p>
@@ -18,7 +18,7 @@ const Hero = () => {
           <img 
             src= {pretzel} 
             alt="baked-goods" 
-            className='h-[60vh] w-[80vw] rounded-xl'
+            className='mx-auto h-[35vh] w-auto md:h-[60vh] md:w-[80vw] rounded-xl'
           />
         </div>
       </div>
